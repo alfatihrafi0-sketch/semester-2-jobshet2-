e70f768be93f46dc30c8e6b823f7bec1dd65eefc
alfatihrafi0-sketch <alfatihrafi0@gmail.com>
2026-02-20 16:13:32 +0700

    jobshet2

--- rafiMain18.java
+++ rafiMain18.java
@@ -15,5 +15,9 @@ public class rafiMain18 {
     rafi18 mhs2 = new rafi18("Annisa Nabila", "2141720160", "TI 2L", 3.25);
      mhs2.updateIpk(3.30);
      mhs2.tampilkanInformasi();
+
+     rafi18 mhs3 = new rafi18("Muhammad Rafialfatih", "254107060134", "SI 1D", 3.75);
+     mhs3.updateIpk(3.90);                                                                                  
+        mhs3.tampilkanInformasi();
     }
 }
