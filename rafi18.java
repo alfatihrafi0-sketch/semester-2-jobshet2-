f10a4e99da7a94622ce99ae36d7af9d69b68db6b
alfatihrafi0-sketch <alfatihrafi0@gmail.com>
2026-02-20 15:10:23 +0700

    jobshet2

---
+++ rafi18.java
@@ -0,0 +1,45 @@
+public class rafi18 {
+    String nama;
+    String nim;
+    String kelas;
+    double ipk; 
+
+public rafi18() {
+}
+
+public rafi18(String nm, String nim, String kls, double ipk) {
+    nama = nm;
+    this.nim = nim;
+    kelas = kls;
+    this.ipk = ipk;
+}
+
+void tampilkanInformasi( ) {
+    System.out.println("Nama: " + nama);
+    System.out.println("NIM: " + nim);
+    System.out.println("IPK: " + ipk);
+    System.out.println("Kelas: " + kelas);
+}
+void ubahKelas (String kelasBaru) {
+    kelas = kelasBaru;
+}
+void updateIpk (double ipkBaru) {
+    if (ipkBaru >= 0.0 && ipkBaru <= 4.0) {
+        ipk = ipkBaru;
+    } else {
+        System.out.println(" IPK tidak valid. Harus antara 0.0 dan 4.0.");
+    }
+}
+String nilaiKinerja () {
+    if (ipk >= 3.5) {
+        return "kinerja Sangat Baik";
+    } else if (ipk >= 3.0) {
+        return "kinerja Baik";
+    } else if (ipk >= 2.0) {
+        return "kinerja Cukup";
+    } else {
+        return "kinerja Kurang";
+    }
+}
+}
+
